c3aa0218332f0602f757e7e1205de913d2a59f2b
Priyanshu Goel <55882904+PriyanshuGoel2000@users.noreply.github.com>
2021-01-19 20:45:18 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -6,6 +6,48 @@ include "connection.php";
 <head>
 </head>
 <body>
+<h3>Registered Users List</h3>
+<table >
+        <thead>
+          <tr>
+            <th >Rid</th>
+            <th >FirstName</th>
+            <th >LastName</th>
+            <th >Email</th>
+            <th >PhoneNumber</th>
+            <th >Gender</th>
+            <th >Age</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+<?php
+	$query="SELECT * FROM register";
+	$result=mysqli_query($conn,$query);
+	$count=mysqli_num_rows($result);
+	while ($row = mysqli_fetch_array($result)) {
+		$type=$row['Rid'];
+		$firstname=$row['firstname'];
+		$lastname=$row['lastname'];
+		$email=$row['email'];
+		$phone=$row['phone'];
+		$gender=$row['gender'];
+		$age=$row['age'];
+		
+		echo "<tr>";
+		echo  " <td> $type</td>";
+		echo " <td> $firstname</td>";
+		echo " <td>$lastname</td>";
+		echo " <td> $email</td>";
+		echo " <td> $phone</td>";
+		echo " <td> $gender</td>";
+		echo " <td>$age</td>";
+		echo "</tr>";
+	}
+	
+		?>
+		</tbody>
+		</table>
 <h3>Contact Responses</h3>
 <table id="contact">
         <thead>
